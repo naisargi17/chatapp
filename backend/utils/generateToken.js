1e6651d6337a9665d61436211c55304ac247b0ac
naishub <patelnaisargi74@gmail.com>
2024-10-28 13:18:14 +0530

    add

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,7 +7,7 @@ const generateTokenAndSetCookie = (userId,res) =>{
         maxAge:15*24*60*60*1000,
         httpOnly:true, //prevent access
         sameSite:"strict",
-        secure: process.env.NODE_ENV !== "developmenyt",
+        secure: "development",
     });
 };
 
